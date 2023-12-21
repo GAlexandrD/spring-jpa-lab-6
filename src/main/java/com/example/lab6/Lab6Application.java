f432a0e2cd3e32663c5afe606eaec034043e3a8c
suxarik <suxarik312004gor@gmail.com>
2023-12-21 15:26:03 +0200

    Add Open Api Definition

--- src/main/java/com/example/lab6/Lab6Application.java
+++ src/main/java/com/example/lab6/Lab6Application.java
@@ -1,8 +1,25 @@
 package com.example.lab6;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+
+				title = "Task Manager API",
+				description = "This is project for Spring labs"
+
+		),
+		servers = {
+
+				@Server(url = "http://localhost:8080", description = "test server"),
+				@Server(url = "http://example.com", description = "production server")
+		}
+
+)
 @SpringBootApplication
 public class Lab6Application {
 
